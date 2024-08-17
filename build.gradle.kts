973715e2bdc4c0ed7564a5471478966e9dce54b7
zooucat <zooucat@gmail.com>
2024-08-17 10:29:29 +0900

    🚀 Set up GitHub Packages repository publishing in Gradle
    - Added 'maven-publish' plugin to build.gradle.kts
    - Configured MavenPublication to publish Java components
    - Set GitHub Packages repository URL for artifact deployment
    - Configured authentication using GitHub Personal Access Token
    - Updated version handling for conflict resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,31 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.2"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("maven-publish")
+}
+
+publishing {
+	publications {
+		create<MavenPublication>("github") {
+			groupId = "zooucat"
+			artifactId = "catwork"
+			version = "1.0.0"
+
+			from(components["java"])
+		}
+	}
+
+	repositories {
+		maven {
+			name = "GitHubPackages"
+			url = uri("https://maven.pkg.github.com/zooucat/catwork")
+
+			credentials {
+				username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+				password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+			}
+		}
+	}
 }
 
 allprojects {
