1924d50d5fe25dd9c2b25ab363bab8697c6f0014
zooucat <zooucat@gmail.com>
2024-08-20 23:38:05 +0900

    🐛 Fix Gradle build configuration and ensure resource inclusion
    - Updated root build.gradle.kts to disable bootJar and enable jar tasks for custom modules.
    - Verified that `catwork-starters` resources are included in the final JAR.
    - Modified submodule build.gradle.kts files to correctly manage dependencies and resources.

--- catwork-autoconfigure/build.gradle.kts
+++ catwork-autoconfigure/build.gradle.kts
@@ -1,2 +1,30 @@
+publishing {
+    publications {
+        create<MavenPublication>("github") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/zooucat/catwork")
+
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_USERNAME")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-autoconfigure:3.3.2")
+}
+
 tasks.register("prepareKotlinBuildScriptModel"){
 }
