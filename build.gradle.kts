b2afe5e9bf87491ffc9275d728ea69437469e8e0
zooucat <zooucat@gmail.com>
2024-08-24 23:12:27 +0900

    📌 Update Spring Boot plugin version to 3.3.3
    - Upgraded the Spring Boot plugin version from 3.3.2 to 3.3.3 for improved stability and bug fixes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
 	id("maven-publish")
-	id("org.springframework.boot") version "3.3.2"
+	id("org.springframework.boot") version "3.3.3"
 	id("io.spring.dependency-management") version "1.1.6"
 }
 
