0bf339cd2ca098294595482bd2e129c0f6dde202
zooucat <zooucat@gmail.com>
2024-08-26 08:54:21 +0900

    🎉 Add new logging module
    - Created a dedicated module for logging functionality.
    - Integrated basic logging configuration and setup.
    - Added support for various log levels and output formats.

--- catwork-logging/src/main/java/catwork/CatworkLoggingApplication.java
+++ catwork-logging/src/main/java/catwork/CatworkLoggingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CatworkDatasourceApplication {
+public class CatworkLoggingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CatworkDatasourceApplication.class, args);
+        SpringApplication.run(CatworkLoggingApplication.class, args);
     }
 
 }
