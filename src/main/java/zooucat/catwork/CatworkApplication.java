feef550ad8b263c5613766c6dc9809e668d41971
zooucat <zooucat@gmail.com>
2025-10-19 13:22:32 +0900

    chore: Initialize Spring Boot project
    - Generated project using Spring Initializr with Spring Boot 3.5.7.
    - Configured to use Java 25.

---
+++ src/main/java/zooucat/catwork/CatworkApplication.java
@@ -0,0 +1,13 @@
+package zooucat.catwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zooucat/catwork/CatworkApplicationTests.java
@@ -0,0 +1,13 @@
+package zooucat.catwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
